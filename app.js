3ad0f2bcbd6a9c5c0bc1a8d70c2c33597cabaebf
Temo <wingsbit@gmail.com>
2023-04-06 16:03:54 +0400

    done

---
+++ app.js
@@ -0,0 +1,9 @@
+let myComputerManufacturer = "Alienware x17 R2";
+let myComputerMemory = "32GB";
+let myComputerOS = "windows 10 Pro 64-bit";
+const computerBuyingYear = 2021;
+let currentYear = 2023;
+let durration = currentYear - computerBuyingYear;
+let sentence = 'my computer is ' + myComputerManufacturer + ', it turns on ' + myComputerOS + " and I've been using it for " + durration + " year."
+console.log(sentence );
+
